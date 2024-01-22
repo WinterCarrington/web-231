e539bae8af06df10fa6362d295adcadebaba4740
WinterCarrington <lwade82@icloud.com>
2024-01-22 10:38:28 -0500

    Added schedule.js and js.03 files, linked js files to HTML file

---
+++ Week 3/js03.js
@@ -0,0 +1,14 @@
+        /*    JavaScript 7th Edition
+            Chapter 3
+            Chapter case
+
+            Tipton Turbines
+            Program to display games results in a web table
+            Author: Lantha Wade        
+            Date:   01/28/24
+
+            Filename: js03.js
+        */
+
+        //Days of the week
+                let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
