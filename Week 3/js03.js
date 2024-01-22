cd6ed94cf10dd8e84f015deef09f72c5bbdda8ee
WinterCarrington <lwade82@icloud.com>
2024-01-22 11:44:54 -0500

    updated js03 file

--- Week 3/js03.js
+++ Week 3/js03.js
@@ -12,3 +12,21 @@
 
         //Days of the week
                 let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                    
+                window.addEventListener("load", addWeekdays);
+
+        //Function to write weekday names into the calendar 
+        function addWeekdays() {
+                let i = 0; //initial counter value
+
+        //Refrence the collection of heading cells in the table
+                let headingCells = document.getElementsByTagName("th");
+
+        //Write each of the seven days into a heading cell
+                while (i < 7) {
+                    headingCells(i).innerHTML = weekDays(i);
+
+        //Increase the counter by 1
+                i++; 
+                }
+         }
