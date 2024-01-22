a7f8c090ef8f473d18705178ce31336557834f47
WinterCarrington <lwade82@icloud.com>
2024-01-22 12:15:53 -0500

    Updated js03 file

--- Week 3/js03.js
+++ Week 3/js03.js
@@ -30,3 +30,30 @@
                 i++; 
                 }
          }
+            window.addEventListener("load", showGames);
+        //funtion to write game information into the calendar 
+         function showGames () {
+            for (let i = 0; i < gameDates.length; i++); {
+                let gameInfo = "";
+
+        //Open the paragraph 
+            gameInfo += "<p>";
+
+        //Include the opponent
+            gameInfo += gameOpponents(i) + "<br>";
+
+        //Include the result and score 
+            gameInfo += gameResults(i) + ": (" + runsScored (i) + "_" + runsAllowed (i) + ")";
+
+        //Close the paragraph 
+            gameInfo += "</p>";
+
+        //Write the information into a table cell 
+            let tableCell = document.getElementById(gameDates (i));
+            tableCell.insertAdjacentHTML("beforeEnd", gameInfo)
+
+            }
+         }
+         
+
+         
