8136f5543080188f2e3f75252c5cef3eea69b092
WinterCarrington <lwade82@icloud.com>
2024-01-22 17:27:21 -0500

    Added contants with initial values to js file

--- Week 3/project02-04.js
+++ Week 3/project02-04.js
@@ -9,6 +9,13 @@
       Filename: project02-04.js
  */
  
+// Constants with initial values
+            const CHICKEN_PRICE = 10.95;
+            const HALIBUT_PRICE = 13.95;
+            const BURGER_PRICE = 9.95;
+            const SALMON_PRICE = 18.95;
+            const SALAD_PRICE = 7.95;
+            const SALES_TAX = 0.07;
 
 
 
