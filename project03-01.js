c4e20379b90b6e80e9b947d29d271465a9b11379
WinterCarrington <lwade82@icloud.com>
2024-02-02 17:06:15 -0500

    updated js and HTML files

--- project03-01.js
+++ project03-01.js
@@ -4,7 +4,7 @@
 
             Application to calculate total order cost
             Author: Lantha Wade   
-            Date:   02/02/24
+            Date:   02/04/24
 
             Filename: project03-01.js
         */
@@ -19,3 +19,13 @@
                 
         // Declare variable menuItems containing HTML elements with the menuItem class
                 var menuItems = document.getElementsByClassName('menuItem');
+                
+        // Loop through the menuItems collection and add event listeners
+                for (var i = 0; i < menuItems.length; i++) {
+                    menuItems[i].addEventListener('click', calcTotal);
+                }
+
+        // Function to calculate total (you may need to define this function)
+                function calcTotal() {
+            
+        }
