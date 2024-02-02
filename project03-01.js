cefcb43fe3b59841513208c25f4ea68557679cbe
WinterCarrington <lwade82@icloud.com>
2024-02-02 16:50:27 -0500

    updated js file to include variable

--- project03-01.js
+++ project03-01.js
@@ -1,19 +1,21 @@
-/*    JavaScript 7th Edition
-      Chapter 3
-      Project 03-01
+        /*    JavaScript 7th Edition
+            Chapter 3
+            Project 03-01
 
-      Application to calculate total order cost
-      Author: Lantha Wade   
-      Date:   02/02/24
+            Application to calculate total order cost
+            Author: Lantha Wade   
+            Date:   02/02/24
 
-      Filename: project03-01.js
-*/
-
-
+            Filename: project03-01.js
+        */
 
 
 
- // Function to display a numeric value as a text string in the format $##.## 
- function formatCurrency(value) {
-    return "$" + value.toFixed(2);
- }
+        // Function to display a numeric value as a text string in the format $##.## 
+                function formatCurrency(value) {
+                    return "$" + value.toFixed(2);
+                }
+
+                
+        // Declare variable menuItems containing HTML elements with the menuItem class
+                var menuItems = document.getElementsByClassName('menuItem');
