a65f8e49e009e57748fb9b31f983168899fa5ef4
WinterCarrington <lwade82@icloud.com>
2024-02-12 15:42:49 -0500

    corrected syntax error on line 18 to properly declare variable

--- Week 6/Chapter/js04_txt.js
+++ Week 6/Chapter/js04_txt.js
@@ -15,7 +15,7 @@
 
 
 /* global variables tracking status of each form section */
-acresComplete = true;
+let acresComplete = true;
 let cropsComplete = true;
 let monthsComplete = true;
 let fuelComplete = true;
