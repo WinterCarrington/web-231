b847122b2cc4e9fd384df785eb788796204892c7
WinterCarrington <lwade82@icloud.com>
2024-02-25 23:21:48 -0500

    updated js file

--- Week 7/project05-01_txt.js
+++ Week 7/project05-01_txt.js
@@ -12,7 +12,7 @@
 
 // Constants to set the time given for the quiz in seconds
 // and the correct answers to each quiz question
-const quizTime = 20;
+const quizTime = 90; // Updated value to 90 seconds
 const correctAnswers = ["10", "4", "-6", "5", "-7"];
 
 // Elements in the quiz page
