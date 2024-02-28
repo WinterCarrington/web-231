de332a6429b3374ec1b8874420bc57d0c3f16f1c
WinterCarrington <lwade82@icloud.com>
2024-02-27 20:44:56 -0500

    Afdded chapter 6 files

---
+++ Week 8/Chapter/js06a_txt.js
@@ -0,0 +1,13 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Chapter case
+
+      Order Form Code
+      Author: Lantha Wade   
+      Date:   03/03/24
+
+      Filename: js06a.js
+ */
+
+
