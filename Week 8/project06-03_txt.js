737cf2d73e750c153dfdbe70981af994577b6dbc
WinterCarrington <lwade82@icloud.com>
2024-02-29 11:50:50 -0500

    Added Hands-on prj 0603

---
+++ Week 8/project06-03_txt.js
@@ -0,0 +1,11 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-03
+
+      Script to complete a form containing billing and shipping address information
+      Author: Lantha Wade
+      Date:   02/29/24
+
+      Filename: project06-03.js
+*/
