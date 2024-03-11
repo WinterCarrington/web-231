8fe877c63da471bb6e032b7da40753ffa38f0700
WinterCarrington <lwade82@icloud.com>
2024-03-10 21:05:20 -0400

    updated js file

--- Week 9/project07-01_txt.js
+++ Week 9/project07-01_txt.js
@@ -13,8 +13,27 @@
 let signupForm = document.getElementById("signup");
 
 signupForm.addEventListener("submit", function(e) { 
+    e.preventDefault();
+    /* add regular expresions */
+    const regex1 = /[A-Z]/;
+    const regex2 = /\d/;
+    const regex3 = /[!$#%.]/;
+  
    let pwd = document.getElementById("pwd").value;
    let feedback = document.getElementById("feedback");
+
+   //Validate the password
+  if (pwd.length < 8) {
+    feedback.textContent = 'Your password must be at least 8 characters.';
+  } else if (!regex1.test(pwd)) {
+    feedback.textContent = 'Your password must include an uppercase letter.';
+  } else if (!regex2.test(pwd)) {
+    feedback.textContent = 'Your password must include a number.';
+  } else if (!regex3.test(pwd)) {
+    feedback.textContent = 'Your password must include one of the following: !$#%.';
+  } else {
+    // All conditions passed, submit the form for processing
+    signupForm.submit();
 
 }
 );
